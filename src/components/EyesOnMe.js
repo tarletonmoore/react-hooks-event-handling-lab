fddf8f57a151882095c85bd60c1afa3474a5fe00
John Tarleton Moore <tarletonmoore@gmail.com>
2022-05-31 10:57:27 -0400

    commit tree c2e0fb0c8ee1bc45011c58311f22c84f67b40358

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+
+
+
+
+function handleFocus() {
+    return (console.log("Good!"))
+
+}
+
+function handleBlur() {
+    return (console.log("Hey! Eyes on me!"))
+
+}
+
+function EyesOnMe() {
+    return (
+        <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+import React from "react"
+
+function handleChange() {
+    return (console.log("Entering password..."))
+}
+
+function Keypad() {
+    return (
+        <input type="password" onChange={handleChange} />
+    )
+}
+
+export default Keypad
